e6fa5fde6f4aa92f95ee3086a10ae0ae2bf540f2
samikshagaiki <samikshagaiki@gmail.com>
2025-03-22 16:00:30 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}" // Adjust based on your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
